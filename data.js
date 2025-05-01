cd6523e392f96b725d828a54358c3a270ee3051a
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-01 15:27:51 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const conteudos = [
+  { titulo: "Justified", url: "/justified/assistir.html" },
+  { titulo: "Breaking Bad", url: "/breakingbad/assistir.html" },
+  { titulo: "The Walking Dead", url: "/walkingdead/assistir.html" },
+  { titulo: "Game of Thrones", url: "/got/assistir.html" },
+];
