711e960b43013281073ae855ab2e702ac62db381
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-02 13:45:10 -0300

    Create series.js

---
+++ categorias/series.js
@@ -0,0 +1,38 @@
+const seriesNovas = [
+  {
+    titulo: "Justified",
+    imagem: "https://assets.fxnetworks.com/cms-next/production/3f013aba-5074-4ca4-a10c-12d88ed4eaa6.jpg",
+    link: "/justified/assistir.html",
+    categoria: "Globo"
+  },
+  {
+    titulo: "Breaking Bad",
+    imagem: "https://infinitasvidas.wordpress.com/wp-content/uploads/2015/10/poster-breaking-bad.png",
+    link: "#",
+    categoria: "Globo"
+  },
+  {
+    titulo: "The Walking Dead",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/a/a2/TheWalkingDead-P%C3%B4ster.jpg",
+    link: "#",
+    categoria: "Globo"
+  },
+  {
+    titulo: "Game of Thrones",
+    imagem: "https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    link: "#",
+    categoria: "Drama"
+  },
+  {
+    titulo: "The Big Bang Theory",
+    imagem: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185554_b_v8_bk.jpg",
+    link: "#",
+    categoria: "Comédia"
+  },
+  {
+    titulo: "Renascer",
+    imagem: "https://s2-techtudo.glbimg.com/lb7JhZXKmVFRVrPZPV0N-99pFiM=/0x0:1200x675/fit-in/640x360/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2024/01/22/renascer.jpg",
+    link: "/renascer/assistir.html",
+    categoria: "Globo"
+  }
+];
