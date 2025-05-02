dc6d49af8f8210ab55b489a182be0b7a2b608158
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-02 13:22:53 -0300

    Update series.js

--- series.js
+++ series.js
@@ -3,19 +3,19 @@ const seriesNovas = [
     titulo: "Justified",
     imagem: "https://assets.fxnetworks.com/cms-next/production/3f013aba-5074-4ca4-a10c-12d88ed4eaa6.jpg",
     link: "/justified/assistir.html",
-    categoria: "Globo"
+    categoria: "Drama"
   },
   {
     titulo: "Breaking Bad",
     imagem: "https://infinitasvidas.wordpress.com/wp-content/uploads/2015/10/poster-breaking-bad.png",
     link: "#",
-    categoria: "Globo"
+    categoria: "Drama"
   },
   {
     titulo: "The Walking Dead",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/a/a2/TheWalkingDead-P%C3%B4ster.jpg",
     link: "#",
-    categoria: "Globo"
+    categoria: "Ação"
   },
   {
     titulo: "Game of Thrones",
@@ -28,5 +28,11 @@ const seriesNovas = [
     imagem: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185554_b_v8_bk.jpg",
     link: "#",
     categoria: "Comédia"
+  },
+  {
+    titulo: "Renascer",
+    imagem: "https://s2-techtudo.glbimg.com/lb7JhZXKmVFRVrPZPV0N-99pFiM=/0x0:1200x675/fit-in/640x360/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2024/01/22/renascer.jpg",
+    link: "/renascer/assistir.html",
+    categoria: "Globo"
   }
 ];
