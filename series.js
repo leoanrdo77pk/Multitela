a97f92f1719f6770bcbc441909e4d235c6797f44
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-02 12:01:23 -0300

    Create series.js

---
+++ series.js
@@ -0,0 +1,28 @@
+// series.js
+const seriesNovas = [
+  {
+    titulo: "Justified",
+    imagem: "https://assets.fxnetworks.com/cms-next/production/3f013aba-5074-4ca4-a10c-12d88ed4eaa6.jpg",
+    link: "/justified/assistir.html"
+  },
+  {
+    titulo: "Breaking Bad",
+    imagem: "https://infinitasvidas.wordpress.com/wp-content/uploads/2015/10/poster-breaking-bad.png",
+    link: "#"
+  },
+  {
+    titulo: "The Walking Dead",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/a/a2/TheWalkingDead-P%C3%B4ster.jpg",
+    link: "#"
+  },
+  {
+    titulo: "Game of Thrones",
+    imagem: "https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    link: "#"
+  },
+  {
+    titulo: "The Big Bang Theory",
+    imagem: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185554_b_v8_bk.jpg",
+    link: "#"
+  }
+];
