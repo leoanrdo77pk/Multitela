0c2e0d21a2dc97639711ddfe916e530e545fa7b8
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-02 13:46:24 -0300

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-const conteudos = [
-  { titulo: "Justified", url: "/justified/assistir.html" },
-  { titulo: "Breaking Bad", url: "/breakingbad/assistir.html" },
-  { titulo: "The Walking Dead", url: "/walkingdead/assistir.html" },
-  { titulo: "Game of Thrones", url: "/got/assistir.html" },
-  { titulo: "A Origem", url: "/got/assistir.html" },
-];
