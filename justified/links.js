893b261b2025de68e02323dda86000a129cfb9cf
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-05 23:36:35 -0300

    Update links.js

--- justified/links.js
+++ justified/links.js
@@ -2,12 +2,12 @@ const temporadas = {
   "Temporada 1": [
     {
       titulo: "Justified S01E01",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727722.mp4",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727760.mp4",
       capa: "http://timg.bdta.pro/images/5b52e3be94b647488fef77b99973a8c1.jpg"
     },
     {
       titulo: "Justified S01E02",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727723.mp4",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727761.mp4",
       capa: "http://timg.bdta.pro/images/06592de5fd33db38910361c0905fe743.jpg"
     },
     {
