66ad55c4d7e10a8e25998ee66bde4db52501ea9e
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-07 10:55:00 -0300

    Update links.js

--- justified/links.js
+++ justified/links.js
@@ -1,35 +1,24 @@
-
-
-const serie = {
-  titulo: "Justified",
-  ano: "2010-2015",
-  genero: "Crime, Drama, Faroeste",
-  sinopse: "O U.S. Marshal Raylan Givens é transferido de Miami para sua terra natal, onde enfrenta criminosos antigos e novos.",
-  capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
-  imdbLink: "https://www.imdb.com/title/tt1486217/" // Link para a série no IMDb
-};
-
 const temporadas = {
   "Temporada 1": [
     {
       titulo: "Justified S01E01",
-       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4397005.mp4",
+      capa: "http://timg.bdta.pro/images/5b52e3be94b647488fef77b99973a8c1.jpg"
     },
     {
       titulo: "Justified S01E02",
-       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
-     capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+      url: "http://lexus.hubns.top:80/Leonardo77/983469871/1256632.m3u8",
+      capa: "http://timg.bdta.pro/images/06592de5fd33db38910361c0905fe743.jpg"
     },
     {
       titulo: "Justified S01E03",
-       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4".
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727724.mp4",
+      capa: "http://timg.bdta.pro/images/39857dee06407e156c40ae6f944dd338.jpg"
     },
     {
       titulo: "Justified S01E04",
-       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727725.mp4",
+      capa: "http://timg.bdta.pro/images/41491cb9eb6fe9e01bc6d8ad8d5c5807.jpg"
     }
   ]
 };
