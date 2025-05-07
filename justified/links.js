9158e6b0a40410df49dc6f66c4a64ac800a84b3e
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-07 10:24:26 -0300

    Update links.js

--- justified/links.js
+++ justified/links.js
@@ -2,21 +2,28 @@ const serie = {
   titulo: "Justified",
   genero: "Crime, Drama, Faroeste",
   sinopse: "O U.S. Marshal Raylan Givens é transferido de Miami para sua terra natal, onde enfrenta criminosos antigos e novos.",
-  capa: "https://m.media-amazon.com/images/M/MV5BNzI3NzZlNjQtZTc3Yi00NzZhLWEzNWMtNzA3ZDVmMWNjMTY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg", // mesma do poster
-  poster: "https://m.media-amazon.com/images/M/MV5BNzI3NzZlNjQtZTc3Yi00NzZhLWEzNWMtNzA3ZDVmMWNjMTY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+  capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // mesma do poster
+  poster: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
   imdb: "https://www.imdb.com/title/tt1486217/"
 };
 
 const temporadas = {
   "Temporada 1": [
     {
-      capa: "https://via.placeholder.com/300x170.png?text=Episodio+1",
-      url: "#"
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432352.mp4"
     },
     {
-      capa: "https://via.placeholder.com/300x170.png?text=Episodio+2",
-      url: "#"
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432353.mp4"
     }
-    // Adicione os outros episódios aqui
+   {
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432354.mp4"
+    },
+    {
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
+      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4"
+    }
   ]
 };
