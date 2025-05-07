c6c41c649977fba5085c0248f9fcec553f016300
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-07 10:10:34 -0300

    Update links.js

--- justified/links.js
+++ justified/links.js
@@ -3,7 +3,7 @@ const serie = {
   ano: "2010-2015",
   genero: "Crime, Drama, Faroeste",
   sinopse: "O U.S. Marshal Raylan Givens é transferido de Miami para sua terra natal, onde enfrenta criminosos antigos e novos.",
-  capa: "https://m.media-amazon.com/images/I/51cA1g6J9tL._AC_.jpg", // Imagem da capa da série (pode ser retirada do IMDb)
+  capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
   imdbLink: "https://www.imdb.com/title/tt1486217/" // Link para a série no IMDb
 };
 
@@ -12,22 +12,22 @@ const temporadas = {
     {
       titulo: "Justified S01E01",
       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4397005.mp4",
-      capa: "https://m.media-amazon.com/images/I/51cA1g6J9tL._AC_.jpg", // Imagem da capa da série (pode ser retirada do IMDb)
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     },
     {
       titulo: "Justified S01E02",
       url: "http://lexus.hubns.top:80/Leonardo77/983469871/1256632.m3u8",
-     capa: "https://m.media-amazon.com/images/I/51cA1g6J9tL._AC_.jpg", // Imagem da capa da série (pode ser retirada do IMDb)
+     capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     },
     {
       titulo: "Justified S01E03",
       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727724.mp4",
-      capa: "https://m.media-amazon.com/images/I/51cA1g6J9tL._AC_.jpg", // Imagem da capa da série (pode ser retirada do IMDb)
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     },
     {
       titulo: "Justified S01E04",
       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727725.mp4",
-      capa: "https://m.media-amazon.com/images/I/51cA1g6J9tL._AC_.jpg", // Imagem da capa da série (pode ser retirada do IMDb)
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     }
   ]
 };
