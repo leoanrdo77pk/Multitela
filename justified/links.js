07696fd4a42400d2f05aca8f4709a0e535302ebb
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-07 10:53:06 -0300

    Update links.js

--- justified/links.js
+++ justified/links.js
@@ -1,29 +1,35 @@
+
+
 const serie = {
   titulo: "Justified",
+  ano: "2010-2015",
   genero: "Crime, Drama, Faroeste",
   sinopse: "O U.S. Marshal Raylan Givens é transferido de Miami para sua terra natal, onde enfrenta criminosos antigos e novos.",
-  capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // mesma do poster
-  poster: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
-  imdb: "https://www.imdb.com/title/tt1486217/"
+  capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+  imdbLink: "https://www.imdb.com/title/tt1486217/" // Link para a série no IMDb
 };
 
 const temporadas = {
   "Temporada 1": [
     {
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432352.mp4"
+      titulo: "Justified S01E01",
+       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     },
     {
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432353.mp4"
-    },
-   {
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432354.mp4"
+      titulo: "Justified S01E02",
+       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
+     capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     },
     {
-      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1",
-      url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4"
+      titulo: "Justified S01E03",
+       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4".
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
+    },
+    {
+      titulo: "Justified S01E04",
+       url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/4432355.mp4",
+      capa: "https://images.justwatch.com/poster/305418324/s332/temporada-1", // Imagem da capa da série (pode ser retirada do IMDb)
     }
   ]
 };
